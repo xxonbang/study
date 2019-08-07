008751adca7843570e16bc7e1fe267d4fa86eb7e
Byeongcheol Son <mackulri@gmail.com>
2019-08-07 17:36:06 +0900

    Lecture #2.8 진행 중

---
+++ bangtube/app.js
@@ -0,0 +1,22 @@
+// const express = require('express')
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+const app = express();
+
+
+const handleHome = (req, res) => res.send("Hello xxonbang!!!!");
+
+const handleProfile = (req, res) => res.send("Profile Page!!!!");
+
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
