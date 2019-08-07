156a8b06cf07275d081428a0c6f19f2de2ae7cf8
Byeongcheol Son <mackulri@gmail.com>
2019-08-07 14:19:33 +0900

    node_modules 들 github 에서 제외하기 위한 commit
    - node_modules files 을 제외하고 내가 작업한 순수한 작업물 files만 저장하기 위함

--- bangtube/index.js
+++ bangtube/index.js
@@ -1,1 +1,23 @@
-console.log('hi');
+const express = require('express')
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+function handleHome(req, res) {
+    res.send("Hello xxonbang!!!!");
+}
+
+function handleProfile(req, res) {
+    res.send("Profile Page!!!!");
+
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
